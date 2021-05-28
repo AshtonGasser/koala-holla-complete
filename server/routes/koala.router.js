30241bf4a2d9c25072b0279c929f92f18ea1c171
Ashton Gasser <ashtongasser@gmail.com>
2021-05-28 11:44:27 -0500

    whooooo

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,21 +2,21 @@ const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 // DB CONNECTION
-const pg = require('pg');
-//pg configuration
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'koala_holla', // THIS CHANGES BY PROJECT
-    host: 'localhost',
-    port: 5432,
-})
-pool.on('connect', () => {
-    console.log('CONNECTED TO POSTGRES');
-});
-pool.on('error', (error) => {
-    console.log(error);
-});
-
+// const pg = require('pg');
+// //pg configuration
+// const Pool = pg.Pool;
+// const pool = new Pool({
+//     database: 'koala_holla', // THIS CHANGES BY PROJECT
+//     host: 'localhost',
+//     port: 5432,
+// })
+// pool.on('connect', () => {
+//     console.log('CONNECTED TO POSTGRES');
+// });
+// pool.on('error', (error) => {
+//     console.log(error);
+// });
+const pool = require('./modules/pool')
 // GET
 koalaRouter.get('/', (req, res) => {
     let queryText = 'SELECT * FROM "koalas" ORDER BY "name";';
